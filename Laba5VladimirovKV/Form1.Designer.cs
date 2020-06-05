eebdc8975f56e650f8778f24826f3cb264f69c42
SuccubHunter <kirilloriginallolo@gmail.com>
2020-06-05 21:11:06 +0500

    Final Build

---
+++ Laba5VladimirovKV/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace Laba5VladimirovKV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Создание файла с числовым массивом";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите кол-во элементов массива";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.groupBox2.Location = new System.Drawing.Point(13, 118);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(224, 539);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Открытие файла";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 88);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(212, 403);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.WordWrap = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.ForeColor = System.Drawing.Color.Firebrick;
+            this.groupBox3.Location = new System.Drawing.Point(242, 118);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(219, 539);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Поиск чисел из диапазона";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(10, 88);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(203, 403);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(107, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "До";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "От";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(135, 36);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(78, 20);
+            this.textBox5.TabIndex = 2;
+            this.textBox5.Text = "100";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(33, 36);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 20);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.Text = "-100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Границы диапазона (включительно)";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 20);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox6.Size = new System.Drawing.Size(212, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Text = "Файл не выбран";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Считанный из файла массив";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Элементы массива из диапазона";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 497);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 36);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Открыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(10, 497);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(203, 36);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Найти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.groupBox6);
+            this.groupBox4.Controls.Add(this.groupBox5);
+            this.groupBox4.ForeColor = System.Drawing.Color.Crimson;
+            this.groupBox4.Location = new System.Drawing.Point(467, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(181, 645);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Дополнительные функции";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.textBox7);
+            this.groupBox5.Controls.Add(this.button4);
+            this.groupBox5.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.groupBox5.Location = new System.Drawing.Point(6, 19);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(169, 308);
+            this.groupBox5.TabIndex = 0;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Чётные или нечётные?";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.textBox8);
+            this.groupBox6.Controls.Add(this.button5);
+            this.groupBox6.ForeColor = System.Drawing.Color.ForestGreen;
+            this.groupBox6.Location = new System.Drawing.Point(6, 333);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(169, 306);
+            this.groupBox6.TabIndex = 1;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Максимальное чётное";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(47, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Выполнить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(6, 48);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox7.Size = new System.Drawing.Size(157, 254);
+            this.textBox7.TabIndex = 1;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(6, 48);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox8.Size = new System.Drawing.Size(157, 254);
+            this.textBox8.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(47, 19);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Выполнить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 669);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Laba5VladimirovKV/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Laba5VladimirovKV
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void groupBox1_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox5_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox2.Clear();// освобождаем textBox2
+			textBox3.Clear(); // освобождаем textBox3
+							  // Создаем объект диалогового окна для открытия файла
+			OpenFileDialog ofd = new OpenFileDialog();
+			//назначаем заголовок диалогового окна открытия файла
+			ofd.Title = "Выберите файл с числовым массивом";
+			ofd.InitialDirectory = @"C:\";// начальный путь для поиска файла
+										  // Создаем фильтр для отображаемых типов файлов
+			ofd.Filter = "txt file (*.txt)|*.txt|all files (*.*)|*.*";
+			// открываем окно для сохранения файла командой ofd.ShowDialog() 
+			//и проверяем, нажата ли кнопка "Открыть" в этом окне, т.е.достигнут ли результат "ОК"
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				using (StreamReader sr = new StreamReader(ofd.FileName)) //открываем файл для чтения
+				{//считываем весь текст из файла и записываем его в textBox2
+					textBox2.Text = sr.ReadToEnd();
+				}
+				textBox6.Text = ofd.FileName;// в textBox6 записываем имя файла
+				button3.Enabled = true; // делаем активной кнопку button3
+				button4.Enabled = true;
+				button5.Enabled = true;
+			}
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox2.Clear(); // освобождаем textBox2
+			textBox3.Clear(); // освобождаем textBox3
+			int n = CorrectInput(textBox1);// получаем числовое значение из textBox1
+			if (n < 1) // проверка допустимости значения, введенного пользователем
+			{
+				n = 1;
+				// замена значения, введенного пользователем в textBox1
+				textBox1.Text = String.Format("{0}", n);
+			}
+			SaveFileDialog sfd = new SaveFileDialog();// Создаем диалоговое окно для открытия файла
+			sfd.Title = "Выберите файл для записи массива";//задаем заголовок диалогового окна
+			sfd.InitialDirectory = @"C:\";// задаем начальный путь для сохранения файла
+										  // Создаем фильтр для отображаемых типов файлов
+			sfd.Filter = "txt file (*.txt)|*.txt|all files (*.*)|*.*";
+			Random r = new Random(); //создаем объект для работы с псевдослучайными числами
+
+			// открываем окно для сохранения файла командой sfd.ShowDialog() 
+			//и проверяем, нажата ли кнопка "Сохранить" в этом окне, т.е.достигнут ли результат "ОК"
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{// открываем файл для записи (дозапись исключена)
+				using (StreamWriter sw = new StreamWriter(sfd.FileName))
+				{
+					for (int i = 0; i < n - 1; i++) // цикл для записи n-1 числа
+						sw.WriteLine(r.Next(-500, 500));  //запись чисел в файл построчно
+														  //запись одного числа в файл без символа перехода на новую строку
+					sw.Write(r.Next(-500, 500));
+				}
+			}
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			textBox3.Clear(); // освобождаем textBox3
+			int min = CorrectInput(textBox4);// получаем числовое значение из textBox4
+			int max = CorrectInput(textBox5);// получаем числовое значение из textBox5
+			if (min > max)// проверка допустимости значения, введенного пользователем
+				textBox3.Text = String.Format("Диапазон задан некорректно");
+			else
+			{
+				int x, k = 0;
+				foreach (string line in textBox2.Lines)// цикл по всем строкам в textBox2
+				{
+					x = Int32.Parse(line); // перевод одной строки line из textBox2 в число х
+					if (x >= min && x <= max)
+					{// добавление строки, содержащей найденное число, в textBox3
+						textBox3.AppendText(line + Environment.NewLine);
+						k++;
+					}
+				}
+				if (k == 0) // проверка, найдено ли хоть одно число в указанном диапазоне
+					textBox3.Text = String.Format("В массиве нет значений, принадлежащих указанному диапазону");
+			}
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			int x;
+			int CountEvens = 0;
+			int CountOdd = 0;
+			foreach (string line in textBox2.Lines)// цикл по всем строкам в textBox2
+			{
+				x = Int32.Parse(line); // перевод одной строки line из textBox2 в число х
+				if ((x%2)==0)
+				{
+					CountEvens++;
+				}
+				else
+				{
+					CountOdd++;
+				}
+			}
+			if (CountEvens > CountOdd)
+			{
+				textBox7.Text = "Больше всего Четных.";
+			}
+			else if(CountOdd > CountEvens)
+			{
+				textBox7.Text = "Больше всего Нечетных.";
+			}
+			else if(CountEvens == CountOdd)
+			{
+				textBox7.Text = "Чётных и Нечётных одинаковое кол-во.";
+			}
+			
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			int x, Max = Int32.Parse(textBox2.Lines[0]);
+			bool EvensCheck = false;
+			foreach (string line in textBox2.Lines)// цикл по всем строкам в textBox2
+			{
+				x = Int32.Parse(line); // перевод одной строки line из textBox2 в число х
+
+				if ((x % 2) == 0)
+				{
+					Max = x;
+					EvensCheck = true;
+					break;
+				}
+			}
+			if (EvensCheck)
+			{
+				foreach (string line in textBox2.Lines)// цикл по всем строкам в textBox2
+				{
+					x = Int32.Parse(line); // перевод одной строки line из textBox2 в число х
+
+					if ((x % 2) == 0)
+					{
+						if (x > Max)
+							Max = x;
+					}
+				}
+				textBox8.Text = "Наибольшая Чётная - " + Max;
+			}
+			else
+			{
+				textBox8.Text = "Чётных нет...";
+			}
+		}
+
+		static int CorrectInput(TextBox TextBox)
+		{
+
+			if (TextBox.Text != "")
+			{
+
+
+				if (IsNum(TextBox.Text))
+				{
+					int Value;
+					Value = Convert.ToInt32(TextBox.Text);
+
+					if (Value < 0) Value = 0;
+					TextBox.Text = String.Format("{0}", Value);
+
+					return Value;
+				}
+				else
+				{
+					TextBox.Text = "0";
+				}
+			}
+
+			return 0;
+		}
+
+		static bool IsNum(string s) //Проверяем строчку состоит она из цифр или других символов
+		{
+			foreach (char c in s)
+			{
+				if (!Char.IsDigit(c)) return false;
+			}
+			return true;
+		}
+	}
+}
